76a325a0035039e7130b0554370c3bb9b89b6d93
Timofey <t.hrynevich@gmail.com>
2026-05-07 18:47:28 +0300

    first form js, some cities

---
+++ scripts/cities.js
@@ -0,0 +1,41 @@
+const cities=[
+    {
+        name:'Minsk',
+        description:'Minsk is the capital and largest city of Belarus, located on the Svislach and the now subterranean Nyamiha rivers',
+        population:'2',
+        images:['',''],
+        attractions:[
+            {
+                name:'National Library off Belarus',
+                description:'The National Library of Belarus is the largest library in the Republic of Belarus, located in Minsk. It houses the largest collection of Belarusian printed materials and the third largest collection of books in Russian',
+                image:''
+            }
+        ]
+    },
+    {
+        name:'Krakow',
+        description:'Krakow , the Royal Capital City of Kraków – a city with county rights located in southern Poland on the Vistula River , second in population',
+        population:'0,8',
+        images:['',''],
+        attractions:[
+            {
+                name:'Main Square',
+                description:'It is the principal urban space located at the center of the city. It dates back to the 13th century, and with an area of 3.79 ha it is sometimes called one of the largest medieval town squares in Europe',
+                image:''
+            }
+        ]
+    },
+    {
+        name:'Tallinn',
+        description:'Tallinn is the capital and most populous city of Estonia. Located on a bay in northern Estonm'pk'kl\[ia, on the shore of the Gulf of Finland of the Baltic Sea',
+        population:'0,5',
+        images:['',''],
+        attractions:[
+            {
+                name:'tallinn old town',
+                description:'Tallinn Old Town is a UNESCO World Heritage site and one of the best-preserved medieval cities in Northern Europe. Located in Estonias capital, it features 13th-16th century cobblestone streets, Gothic spires, and historic city walls. ',
+                image:''
+            }
+        ]
+    }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,31 @@
-const cities=[
-    {
-        name:'Minsk',
-        description:'Minsk is the capital and largest city of Belarus, located on the Svislach and the now subterranean Nyamiha rivers',
-        population:'2',
-        images:['',''],
-        attractions:[
-            {
-                name:'National Library off Belarus',
-                description:'The National Library of Belarus is the largest library in the Republic of Belarus, located in Minsk. It houses the largest collection of Belarusian printed materials and the third largest collection of books in Russian',
-                image:''
-            }
-        ]
-    },
-    {
-        name:'Krakow',
-        description:'Krakow , the Royal Capital City of Kraków – a city with county rights located in southern Poland on the Vistula River , second in population',
-        population:'0,8',
-        images:['',''],
-        attractions:[
-            {
-                name:'Main Square',
-                description:'It is the principal urban space located at the center of the city. It dates back to the 13th century, and with an area of 3.79 ha it is sometimes called one of the largest medieval town squares in Europe',
-                image:''
-            }
-        ]
-    },
-    {
-        name:'Tallinn',
-        description:'Tallinn is the capital and most populous city of Estonia. Located on a bay in northern Estonia, on the shore of the Gulf of Finland of the Baltic Sea',
-        population:'0,5',
-        images:['',''],
-        attractions:[
-            {
-                name:'tallinn old town',
-                description:'Tallinn Old Town is a UNESCO World Heritage site and one of the best-preserved medieval cities in Northern Europe. Located in Estonias capital, it features 13th-16th century cobblestone streets, Gothic spires, and historic city walls. ',
-                image:''
-            }
-        ]
-    }
-];
+
+const form = document.querySelector('.trip-form');
+
+
+form.addEventListener("submit", async (e) => { 
+    e.preventDefault();
+
+    const fd = new FormData(form);
+    
+    
+    const payload = {
+        tripType: fd.get("trip-type"),
+        from: fd.get("from"),
+        to: fd.get("to"),
+        depart: fd.get("depart"),
+        return: fd.get("return"),
+        adults: Number(fd.get("adults")),
+        children: Number(fd.get("children")),
+        infants: Number(fd.get("infants")), 
+        class: fd.get("class-of-travel")
+    };
+
+        //     const r = await fetch("/", {
+        //     method: "POST",
+        //     headers: { "Content-Type": "application/json" },
+        //     body: JSON.stringify(payload), 
+        // });
+    console.log("формa:", payload);
+   
+});
+  
