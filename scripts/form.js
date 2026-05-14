7c3c54ec9abd96fc0990c0e0d2b1061723ea884c
Timofey <t.hrynevich@gmail.com>
2026-05-14 18:52:06 +0300

    server.js

--- scripts/form.js
+++ scripts/form.js
@@ -1,18 +1,56 @@
-
-const form = document.querySelector('.userForm');
-
-
-form.addEventListener("submit", async (e) => { 
-    e.preventDefault();
-
-    const fd = new FormData(form);
+const registration = async (event) => {
+    event.preventDefault();
+  
     
-    const payload = Object.fromEntries(fd);
-        
-    if (payload.password !== payload.coniformPassword) {
-        console.log("Пароли не совпадают!");
-        return;
-    }
-    console.log("формa:", payload);
+    const name = document.getElementById("name").value;
+    const surname = document.getElementById("surname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("coniform_password").value;
+  
    
-});
+    if (password !== confirmPassword ) {
+      alert("Пароли не совпадают!");
+      return;
+    }
+
+    if (password.length < 8 ) {
+        alert("Пароль слишком короткий!");
+        return;
+      }
+      try {
+        const response = await fetch("/registration", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            surname,
+            email,
+            password,
+            confirmPassword
+          }),
+        });
+    
+  
+    const data = await response.json();
+  
+    console.log(data);
+  
+   
+    if (!response.ok) {
+     throw new Error(data.message);
+    }
+    alert("Регистрация успешна");
+
+} catch (error) {
+  console.error(error);
+  alert("Ошибка регистрации");
+}
+};
+
+  document
+    .querySelector(".userForm")
+    .addEventListener("submit", registration);
+  
